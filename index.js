15065ca2a624875a3f6d5ff28bca2a7e4b12a8bf
springis21 <57431871+springis21@users.noreply.github.com>
2020-06-22 14:38:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like IU and NEW!');
+    res.send('I like IU and NEW FACE!');
 });
 
 app.get('/start', function(req, res){
